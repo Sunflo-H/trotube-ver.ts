51135dfb80548fb0628e8251c1522cf55ee021f0
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-03-19 00:18:01 +0900

    reset HEAD^ 후 다시 커밋 이후에 revert를 사용할것임

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Outlet } from "react-router-dom";
-import SearchHeader from "./components/header/SearchHeader";
-import { Provider } from "react-redux";
-import store from "./store";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {Outlet} from 'react-router-dom';
+import SearchHeader from './components/header/SearchHeader';
+import {Provider} from 'react-redux';
+import store from './store';
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <>
-      <Provider store={store}>
+      hello TS
+      {/* <Provider store={store}>
         <SearchHeader />
         <QueryClientProvider client={queryClient}>
           <Outlet />
         </QueryClientProvider>
-      </Provider>
+      </Provider> */}
     </>
   );
 }
