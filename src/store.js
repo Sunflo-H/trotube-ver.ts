3e29d2903131ab72977a16d30aecbc8a76ebf16c
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-03-20 23:27:01 +0900

    Delete src directory

--- src/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import searchReducer from "./features/search/searchSlice";
-
-export default configureStore({
-  reducer: { search: searchReducer },
-});
