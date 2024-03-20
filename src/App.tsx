fef8c7ac0aed185d92a175dbc4be4bda586ef9ce
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-03-20 23:54:11 +0900

    리액트 + 타입스크립트 + 깃 초기 세팅 완료

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">타입스크립트</div>;
+  return <div className="App">리액트 + 타입스크립트 + 깃 초기 세팅 완료</div>;
 }
 
 export default App;
