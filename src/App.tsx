445aec084a7a0e9356568d5f0f591639cbdbfb0e
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-03-22 22:41:26 +0900

    Delete src directory

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-  return <div className="App">리액트 + 타입스크립트 + 깃 초기 세팅 완료</div>;
-}
-
-export default App;
