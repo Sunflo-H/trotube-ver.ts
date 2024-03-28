871f894b871201c5efee8286b74cbfebdc724362
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-03-28 22:46:07 +0900

    Feat : tailwindcss 설치 및 설정 #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "./App.css";
-import { Address, Restaurant } from "./userTypes/restaurant";
 
 import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
