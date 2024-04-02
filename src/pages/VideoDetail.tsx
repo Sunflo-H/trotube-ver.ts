6e0a2646acf47fb92c050bae0df531e34ab27030
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2024-04-02 20:44:31 +0900

    Feat : <VideoDetail> 복사 #4

---
+++ src/pages/VideoDetail.tsx
@@ -0,0 +1,80 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+// import Channel from "../components/main/VideoDetailPage/Channel";
+// import Description from "../components/main/VideoDetailPage/Description";
+// import RelatedVideoCard from "../components/main/VideoDetailPage/RelatedVideoCard";
+// import { getRelatedVideos } from "../queryFn/youtubeQueries";
+
+const VideoDetail: React.FC = () => {
+  const [show, setShow] = useState(false);
+  const {
+    state: { video },
+  } = useLocation();
+  const { channelId, description, title } = video.snippet;
+
+  const { data: relatedVideos } = useQuery({
+    queryKey: ["relatedVideos", video.id, title],
+    // queryFn: getRelatedVideos,
+  });
+
+  // 새 비디오디테일 페이지로 이동했을때 show state를 초기화한다.
+  useEffect(() => {
+    setShow(false);
+  }, [video]);
+
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col lg:flex-row max-w-screen-2xl ">
+        <section className="basis-9/12 sm:px-2">
+          <iframe
+            className="hidden sm:block"
+            id="player"
+            // type="text/html"
+            width="100%"
+            height="640"
+            src={`https://www.youtube.com/embed/${video.id}`}
+            frameBorder="0"
+            title={title}
+            allow="fullscreen"
+          ></iframe>
+          <iframe
+            className=" sm:hidden px-2"
+            id="player"
+            // type="text/html"
+            width="100%"
+            height="300px"
+            src={`https://www.youtube.com/embed/${video.id}`}
+            frameBorder="0"
+            title={title}
+            allow="fullscreen"
+          ></iframe>
+          <div className="w-full">
+            <div className="px-2 text-xl font-semibold my-4">{title}</div>
+            {/* <Channel channelId={channelId} /> */}
+
+            {/* {description && (
+              <Description
+                description={description}
+                show={show}
+                setShow={setShow}
+              />
+            )} */}
+          </div>
+        </section>
+        {/* <section className="basis-3/12 px-2">
+          {relatedVideos && (
+            <ul>
+              {relatedVideos.map((video) => (
+                <RelatedVideoCard video={video} key={video.id} />
+              ))}
+            </ul>
+          )}
+        </section> */}
+      </div>
+    </div>
+  );
+};
+
+export default VideoDetail;
